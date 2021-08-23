c2b5e754297289e82b435436af62b445abe4c2c6
Cluster-aslee <dispink@icloud.com>
2021-08-23 12:29:00 +0200

    Find the optimal parameters for NMF+SVR
    grid_svr.py is modified to be suitable for fine-tuning searching grids.
    The desired measurement for grid searching can be asigned when executing
    grid_sve.py. The model dumping is temparily disabled.
    
    build_models_08.ipynb: Records the process of finding the optimal
    parameters and the optimal parameters for CaCO3 and TOC.
    
    build_models_09.ipynb: Summarize the results from build_models_08.ipynb.
    
    Outputs:
    grid_caco3+toc_compile_20210823.csv
    toc_grid_13nmf+svr_20210823.png
    caco3_grid_4nmf+svr_20210823.png
    caco3/toc_scores_components_20210823.png
    Detailed grid csvs are written in build_models_08.ipynb
    	new file:   build_models_08.ipynb
    	new file:   build_models_09.ipynb
    	modified:   grid_svr.py
    	modified:   submit_svr.sh

--- grid_svr.py
+++ grid_svr.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np 
 import pandas as pd
 from nwp_cali import PrepareData
@@ -12,31 +13,34 @@ date = datetime.datetime.now().strftime('%Y%m%d')
 
 from time import perf_counter
 path = '/home/users/aslee/CaCO3_NWP/'
-for measurement in ['CaCO3%', 'TOC%']:
-    start = perf_counter()
 
-    prepare = PrepareData(measurement=measurement)
-    X, y = prepare.produce_Xy(prepare.select_data())
-    X_train, X_dev, y_train, y_dev = train_test_split(X, y, test_size = 0.2, shuffle = True, random_state = 24)
+measurement = sys.argv[1]
+# mark the time of increasing grid
+run = sys.argv[2]
+start = perf_counter()
 
-    print('Begin NMF+SVR: {}'.format(measurement))
+prepare = PrepareData(measurement=measurement)
+X, y = prepare.produce_Xy(prepare.select_data())
+X_train, X_dev, y_train, y_dev = train_test_split(X, y, test_size = 0.2, shuffle = True, random_state = 24)
 
-    pipe = make_pipeline(NMF(max_iter = 8000, random_state = 24), SVR())
-    params = {
-        'nmf__n_components': range(5, 11),
-        'svr__C': np.logspace(2, 8, 7),
-        'svr__gamma': np.logspace(-4, 2, 7)
-    }
-    grid = GridSearchCV(pipe, param_grid = params, cv = 10, n_jobs = -1)  
+print('Begin NMF+SVR: {}'.format(measurement))
 
-    grid.fit(X_train, np.log(y_train))
+pipe = make_pipeline(NMF(max_iter = 8000, random_state = 24), SVR())
+params = {
+    'nmf__n_components': [3],
+    'svr__C': np.logspace(-2, -1, 2),
+    'svr__gamma': np.logspace(1, 5, 5)
+}
+grid = GridSearchCV(pipe, param_grid = params, cv = 10, n_jobs = -1)  
 
-    print('The best cv score: {:.3f}'.format(grid.best_score_)) 
-    print('The best model\'s parameters: {}'.format(grid.best_estimator_))
+grid.fit(X_train, np.log(y_train))
 
-    pd.DataFrame(grid.cv_results_).to_csv('{}results/caco3_grid_nmf+svr_{}.csv'.format(path, date))
+print('The best cv score: {:.3f}'.format(grid.best_score_)) 
+print('The best model\'s parameters: {}'.format(grid.best_estimator_))
 
-    from joblib import dump, load
-    dump(grid.best_estimator_, '{}models/caco3_nmf+svr_model_{}.joblib'.format(path, date)) 
+pd.DataFrame(grid.cv_results_).to_csv('{}results/{}_grid_nmf+svr_{}_{}.csv'.format(path, measurement[:-1].lower(), run, date))
 
-    print("The computation takes {} hours.".format((perf_counter() - start)/3600))
+#from joblib import dump
+#dump(grid.best_estimator_, '{}models/{}_nmf+svr_model_{}.joblib'.format(path, measurement[:-1].lower(), date)) 
+
+print("The computation takes {} hours.".format((perf_counter() - start)/3600))
--- submit_svr.sh
+++ submit_svr.sh
@@ -4,10 +4,10 @@
 #SBATCH --qos normal
 #SBATCH --output=/home/users/aslee/CaCO3_NWP/job_logs/slurm-%j.txt
 #SBATCH -c 64
-#SBATCH --mem=200GB
-#SBATCH -t 24:00:00
+#SBATCH --mem=50GB
+#SBATCH -t 20:00:00
 
 
 #/home/users/aslee/miniconda3/bin/python /home/users/aslee/CaCO3_NWP/grid_toc.py
 #/home/users/aslee/miniconda3/bin/python /home/users/aslee/CaCO3_NWP/grid_caco3.py
-/home/users/aslee/miniconda3/envs/caco3/bin/python /home/users/aslee/CaCO3_NWP/grid_svr.py
+/home/users/aslee/miniconda3/envs/caco3/bin/python /home/users/aslee/CaCO3_NWP/grid_svr.py CaCO3% 2-1
