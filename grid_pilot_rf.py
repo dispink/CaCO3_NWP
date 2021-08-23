b8631a387f120417affa0c48b13aa447022aae68
Cluster-aslee <dispink@icloud.com>
2021-08-23 05:32:45 +0200

    Include Ridge and Randomforest into the pilot test
    The Summary is modified based on the new findings.
    
    Outputs:
    pilot_grid_ridge_20210823.csv
    pilot_grid_rf_20210822.csv
    	new file:   grid_pilot_rf.py
    	modified:   grid_pilot_ridge.py
    	modified:   pilot_test.ipynb
    	modified:   submit_pilot.sh

--- grid_pilot_rf.py
+++ grid_pilot_rf.py
@@ -1,4 +1,3 @@
-import numpy as np 
 import pandas as pd
 
 import datetime
@@ -15,34 +14,37 @@ X = data_df.iloc[:, 1: -5].values
 X = X / X.sum(axis = 1, keepdims = True)
 y = data_df.caco3_percent.values
 
-print('Begin: Pilot Ridge')
-
-from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.decomposition import NMF
-from sklearn.linear_model import Ridge
-from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
 
-pipe = Pipeline([('scaling', StandardScaler()),('preprocessing', PCA()), ('ridge', Ridge())])
+print('Begin: Pilot RF')
+
+pipe = Pipeline([('scaling', StandardScaler()),('preprocessing', PCA()), 
+                 ('rf', RandomForestRegressor(random_state=24))])
 
 param_grid = [
     {'scaling': [StandardScaler()],
      'preprocessing': [PCA(n_components=5, whiten=True)],
-     'ridge__alpha': np.logspace(-8, 1, 10)},
+     'rf__n_estimators': [100, 1000, 10000],
+     'rf__max_depth': [5, 10, 20, 30]},
     {'scaling': [None],
      'preprocessing': [NMF(n_components=5, random_state=24, max_iter=4000)],
-     'ridge__alpha': np.logspace(-8, 1, 10)}
+     'rf__n_estimators': [100, 1000, 10000],
+     'rf__max_depth': [5, 10, 20, 30]}
 ]
 
-grid = GridSearchCV(pipe, param_grid = param_grid, cv = 5, n_jobs = -1, 
-                    return_train_score = False)  
+grid = GridSearchCV(pipe, param_grid=param_grid, cv=5, 
+                    n_jobs=-1, return_train_score=False)  
 
 grid.fit(X, y)
 
 print('The best cv score: {:.3f}'.format(grid.best_score_)) 
 print('The best model\'s parameters: {}'.format(grid.best_estimator_))
 
-pd.DataFrame(grid.cv_results_).to_csv('{}results/pilot_grid_ridge_{}.csv'.format(path, date))
+pd.DataFrame(grid.cv_results_).to_csv('{}results/pilot_grid_rf_{}.csv'.format(path, date))
 
-print("The computation takes {} mins.".format((perf_counter() - start)/60))
+print("The computation takes {} hours.".format((perf_counter() - start)/3600))
--- submit_pilot.sh
+++ submit_pilot.sh
@@ -3,11 +3,12 @@
 #SBATCH -J grid_pilot
 #SBATCH --qos normal
 #SBATCH --output=/home/users/aslee/CaCO3_NWP/job_logs/slurm-%j.txt
-#SBATCH --hint=multithread
-#SBATCH --nodes=1 --exclusive --cpus-per-task=1 --ntasks-per-node=64
-#SBATCH --mem=240GB
-#SBATCH -t 1:00:00
+#SBATCH -c 64
+#SBATCH --mem=50GB
+#SBATCH -t 20:00:00
 
 
-/home/users/aslee/miniconda3/bin/python /home/users/aslee/CaCO3_NWP/grid_pilot_ridge.py
+#/home/users/aslee/miniconda3/bin/python /home/users/aslee/CaCO3_NWP/grid_pilot_ridge.py
 #/home/users/aslee/miniconda3/bin/python /home/users/aslee/CaCO3_NWP/grid_pilot_svr.py
+#/home/users/aslee/miniconda3/envs/caco3/bin/python /home/users/aslee/CaCO3_NWP/grid_pilot_rf.py
+/home/users/aslee/miniconda3/envs/caco3/bin/python /home/users/aslee/CaCO3_NWP/grid_pilot_ridge.py
